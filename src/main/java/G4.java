5747426c75af361c4abbd58a1f9951ae7e3d1598
MagnusHagerstrom <magnus.hagerstrom@gmail.com>
2026-08-24 18:49:55 +0200

    Added G4 Added lines 2-14 in G4

---
+++ src/main/java/G4.java
@@ -0,0 +1,17 @@
+public class G4 {
+    public static void main(String[] args) {
+
+        System.out.println("Mina 3 favoritband:");
+        System.out.println("1. In Flames.");
+        System.out.println("2. LångfingerBand");
+        System.out.println("3. Worthwhile.");
+        System.out.println("Måste också nämna dessa band, annars känns allt fel ;D");
+        System.out.println("Slomosa");
+        System.out.println("Kal-El");
+        System.out.println("Tiny moving parts");
+        System.out.println("Truckfighters");
+        System.out.println("Telekinesis");
+        System.out.println("Raised Fist");
+    }
+
+}
