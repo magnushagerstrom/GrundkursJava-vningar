7a6a85d6ca62355f9d871e110de5995b4b07a647
MagnusHagerstrom <magnus.hagerstrom@gmail.com>
2026-08-24 18:31:06 +0200

    Added lines 2-7 in G2

--- src/main/java/G2.java
+++ src/main/java/G2.java
@@ -1,2 +1,9 @@
 public class G2 {
+    public static void main(String[] args) {
+
+        System.out.println("Namn: Magnus Hägerström.");
+        System.out.println("Stad: Norrtälje");
+        System.out.println("Jag går utbildningen för att jag vill byta karriär.");
+        System.out.println("Kul fakta: Jag kommer egentligen från Sundsvall, flyttade till Norrtälje för 6 år sedan.");
+    }
 }
