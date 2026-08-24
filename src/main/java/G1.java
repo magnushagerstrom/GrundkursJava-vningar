c58199eb174f4f6b87dcc78230ef7b426072fa30
MagnusHagerstrom <magnus.hagerstrom@gmail.com>
2026-08-24 18:21:00 +0200

    Added G1 and G2. Added lines 2-6 in G1

---
+++ src/main/java/G1.java
@@ -0,0 +1,8 @@
+public class G1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hej Världen!");
+        System.out.println("Mitt namn är Magnus");
+        System.out.println("Dagens datum är 2026-08-24");
+    }
+}
