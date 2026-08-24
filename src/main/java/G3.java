d088131e6f68419226ca036faa1d840cc6e26a50
MagnusHagerstrom <magnus.hagerstrom@gmail.com>
2026-08-24 18:35:42 +0200

    Added G3 Added lines 2-7 in G3

---
+++ src/main/java/G3.java
@@ -0,0 +1,9 @@
+public class G3 {
+    public static void main(String[] args) {
+
+        System.out.println("Kurs: Programmering med Java, grund.");
+        System.out.println("Längd: 8 veckor.");
+        System.out.println("Antal inlämningar: 2st.");
+        System.out.println("Inlämning 1 deadline: Söndag 20 September.");
+    }
+}
