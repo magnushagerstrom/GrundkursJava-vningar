9f6cdbfc5eb08da50249faa4d86cf81762fe4334
MagnusHagerstrom <magnus.hagerstrom@gmail.com>
2026-08-24 19:27:35 +0200

    Added G7 Added lines 2-7 in G7

---
+++ src/main/java/G7.java
@@ -0,0 +1,9 @@
+public class G7 {
+    public static void main(String[] args) {
+
+        System.out.println("Magnus Hägerström");
+        System.out.println("Dragontorpsvägen 18");
+        System.out.println("76175 NORRTÄLJE");
+        System.out.println("Sverige");
+    }
+}
