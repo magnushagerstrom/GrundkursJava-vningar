392aa5f6a5b517ea8d5acdb9505f34f70b9c82f2
MagnusHagerstrom <magnus.hagerstrom@gmail.com>
2026-08-24 19:07:47 +0200

    Added G5 Added lines 2-5 in G5

---
+++ src/main/java/G5.java
@@ -0,0 +1,7 @@
+public class G5 {
+    public static void main(String[] args) {
+        /* Jag har medvetet skrivit med \n för att flexa lite. Sparar man 1 rad kod. */
+        System.out.println("VECKA 1 - KOM IGÅNG\n-------------------");
+        System.out.println("Idag installerade vi allt vi behöver.");
+    }
+}
