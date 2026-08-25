adf6fd93c343eae6dba47798d589f202f2d21ee0
MagnusHagerstrom <magnus.hagerstrom@gmail.com>
2026-08-25 11:41:31 +0200

    Added VG3 and lines 2-7

---
+++ src/main/java/VG3.java
@@ -0,0 +1,9 @@
+public class VG3 {
+    public static void main(String[] args) {
+        System.out.println("==================================\n" +
+                "Magnus Hägerström\n" +
+                "Java-utvecklare (under utbildning)\n" +
+                "github.com/magnushagerstrom\n" +
+                "==================================");
+    }
+}
