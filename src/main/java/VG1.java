f6b1796ddfe9654539f9b4e2c01154d89230b081
MagnusHagerstrom <magnus.hagerstrom@gmail.com>
2026-08-25 11:32:56 +0200

    Added VG2 Added lines 2-8 in VG2

---
+++ src/main/java/VG1.java
@@ -0,0 +1,8 @@
+public class VG1 {
+    public static void main(String[] args) {
+
+        System.out.println("+----------------+\n" +
+                "| Hej världen! |\n" +
+                "+----------------+");
+    }
+}
---
+++ src/main/java/VG2.java
@@ -0,0 +1,10 @@
+public class VG2 {
+    public static void main(String[] args) {
+        System.out.println("*\n" +
+                "**\n" +
+                "***\n" +
+                "****\n" +
+                "*****");
+
+    }
+}
