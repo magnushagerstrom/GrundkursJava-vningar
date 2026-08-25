62942504092d7a97f803037ad4e6a0e0095fd817
MagnusHagerstrom <magnus.hagerstrom@gmail.com>
2026-08-25 11:46:28 +0200

    Added VG4 and lines 2-9

---
+++ src/main/java/VG4.java
@@ -0,0 +1,11 @@
+public class VG4 {
+    public static void main(String[] args) {
+        System.out.println("KVITTO - Hägers Kiosk\n" +
+                "---------------------\n" +
+                "Kaffe 25 kr\n" +
+                "Smörgås 45 kr\n" +
+                "Äpple 10 kr\n" +
+                "---------------------\n" +
+                "Summa 80 kr");
+    }
+}
